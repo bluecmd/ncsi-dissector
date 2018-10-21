226c0114d0d0cb2c77f1ca3cdfd1519688d9c9a9
Christian Svensson <bluecmd@google.com>
2018-10-21 16:30:08 +0200

    Add all other NCSI commands
    Signed-off-by: Christian Svensson <bluecmd@google.com>

--- packet-ncsi.c
+++ packet-ncsi.c
@@ -19,8 +19,33 @@
 #include <epan/prefs.h>
 #include <epan/expert.h>
 
-#define NCSI_PKT_OEM_REQ  0x50
-#define NCSI_PKT_OEM_RESP NCSI_PKT_OEM_REQ + 128
+#define NCSI_CMD_CIS   0x00
+#define NCSI_CMD_SP    0x01
+#define NCSI_CMD_DP    0x02
+#define NCSI_CMD_EC    0x03
+#define NCSI_CMD_DC    0x04
+#define NCSI_CMD_RC    0x05
+#define NCSI_CMD_ECNTX 0x06
+#define NCSI_CMD_DCNTX 0x07
+#define NCSI_CMD_AEN   0x08
+#define NCSI_CMD_SL    0x09
+#define NCSI_CMD_GLS   0x0A
+#define NCSI_CMD_SVF   0x0B
+#define NCSI_CMD_EV    0x0C
+#define NCSI_CMD_DV    0x0D
+#define NCSI_CMD_SMA   0x0E
+#define NCSI_CMD_EBF   0x10
+#define NCSI_CMD_DBF   0x11
+#define NCSI_CMD_EGMF  0x12
+#define NCSI_CMD_DGMF  0x13
+#define NCSI_CMD_SFC   0x14
+#define NCSI_CMD_GVID  0x15
+#define NCSI_CMD_GC    0x16
+#define NCSI_CMD_GP    0x17
+#define NCSI_CMD_GCPS  0x18
+#define NCSI_CMD_GS    0x19
+#define NCSI_CMD_GPS   0x1A
+#define NCSI_CMD_OEM   0x50
 
 void proto_register_ncsi(void);
 
@@ -69,7 +94,7 @@ dissect_ncsi(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
 
   offset = 16;
 
-  if (cmd > 128) {
+  if (cmd > 0x7f) {
     // Decode command response/reason that are present on all responses
     proto_tree_add_item(ncsi_tree, hf_ncsi_cmd_response, tvb, offset, 2, ENC_BIG_ENDIAN);
     proto_tree_add_item(ncsi_tree, hf_ncsi_cmd_reason, tvb, offset + 2, 2, ENC_BIG_ENDIAN);
@@ -153,6 +178,422 @@ proto_register_ncsi(void)
 }
 
 static int
+dissect_ncsi_cis_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Clear Initial State request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_cis_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Clear Initial State response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sp_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Select Package request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sp_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Select Package response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dp_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Deselect Package request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dp_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Deselect Package response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ec_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Channel request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ec_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Channel response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dc_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Channel request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dc_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Channel response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_rc_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Reset Channel request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_rc_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Reset Channel response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ecntx_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Channel Network TX request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ecntx_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Channel Network TX response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dcntx_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Channel Network TX request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dcntx_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Channel Network TX response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_aen_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "AEN Enable request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_aen_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "AEN Enable response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sl_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set Link request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sl_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set Link response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gls_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Link Status request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gls_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Link Status response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_svf_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set VLAN Filter request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_svf_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set VLAN Filter response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ev_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable VLAN request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ev_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable VLAN response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dv_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable VLAN request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dv_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable VLAN response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sma_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set MAC dAdress request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sma_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set MAC Address response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ebf_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Broadcast Filtering request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_ebf_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Broadcast Filtering response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dbf_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Broadcast Filtering request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dbf_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Broadcast Filtering response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_egmf_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Global Multicast Filtering request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_egmf_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Enable Global Multicast Filtering response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dgmf_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Global Multicast Filtering request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_dgmf_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Disable Global Multicast Filtering response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sfc_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set NC-SI Flow Control request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_sfc_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Set NC-SI Flow Control response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gvid_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Version ID request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gvid_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Version ID response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gc_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Capabilities request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gc_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Capabilities response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gp_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Parameters request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gp_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Parameters response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gcps_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Controller Packet Statistics request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gcps_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get Controller Packet Statistics response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gs_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get NC-SI Statistics request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gs_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get NC-SI Statistics response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gps_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get NC-SI Pass-through Statistics request");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
+dissect_ncsi_gps_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
+{
+  col_append_str(pinfo->cinfo, COL_INFO, "Get NC-SI Pass-through Statistics response");
+  /* TODO(bluecmd): Implement */
+  return tvb_captured_length(tvb);
+}
+
+static int
 dissect_ncsi_oem_resp(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, void *data _U_)
 {
   proto_item *ti;
@@ -179,16 +620,48 @@ dissect_ncsi_oem_req(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_
   return tvb_captured_length(tvb);
 }
 
+static void
+reg_ncsi_cmd(guint32 cmd, dissector_t req, dissector_t resp)
+{
+  dissector_handle_t command_handle;
+  command_handle = create_dissector_handle(req, proto_ncsi);
+  dissector_add_uint("ncsi.ctrl_pkt_type", cmd, command_handle);
+  command_handle = create_dissector_handle(resp, proto_ncsi);
+  dissector_add_uint("ncsi.ctrl_pkt_type", cmd + 0x80, command_handle);
+}
+
 void
 proto_reg_handoff_ncsi(void)
 {
-  dissector_handle_t command_handle;
   dissector_add_uint("ethertype", ETHERTYPE_NCSI, ncsi_handle);
 
-  command_handle = create_dissector_handle(dissect_ncsi_oem_resp, proto_ncsi);
-  dissector_add_uint("ncsi.ctrl_pkt_type", NCSI_PKT_OEM_RESP, command_handle);
-  command_handle = create_dissector_handle(dissect_ncsi_oem_req, proto_ncsi);
-  dissector_add_uint("ncsi.ctrl_pkt_type", NCSI_PKT_OEM_REQ, command_handle);
+  reg_ncsi_cmd(NCSI_CMD_CIS, dissect_ncsi_cis_req, dissect_ncsi_cis_resp);
+  reg_ncsi_cmd(NCSI_CMD_SP, dissect_ncsi_sp_req, dissect_ncsi_sp_resp);
+  reg_ncsi_cmd(NCSI_CMD_DP, dissect_ncsi_dp_req, dissect_ncsi_dp_resp);
+  reg_ncsi_cmd(NCSI_CMD_EC, dissect_ncsi_ec_req, dissect_ncsi_ec_resp);
+  reg_ncsi_cmd(NCSI_CMD_DC, dissect_ncsi_dc_req, dissect_ncsi_dc_resp);
+  reg_ncsi_cmd(NCSI_CMD_RC, dissect_ncsi_rc_req, dissect_ncsi_rc_resp);
+  reg_ncsi_cmd(NCSI_CMD_ECNTX, dissect_ncsi_ecntx_req, dissect_ncsi_ecntx_resp);
+  reg_ncsi_cmd(NCSI_CMD_DCNTX, dissect_ncsi_dcntx_req, dissect_ncsi_dcntx_resp);
+  reg_ncsi_cmd(NCSI_CMD_AEN, dissect_ncsi_aen_req, dissect_ncsi_aen_resp);
+  reg_ncsi_cmd(NCSI_CMD_SL, dissect_ncsi_sl_req, dissect_ncsi_sl_resp);
+  reg_ncsi_cmd(NCSI_CMD_GLS, dissect_ncsi_gls_req, dissect_ncsi_gls_resp);
+  reg_ncsi_cmd(NCSI_CMD_SVF, dissect_ncsi_svf_req, dissect_ncsi_svf_resp);
+  reg_ncsi_cmd(NCSI_CMD_EV, dissect_ncsi_ev_req, dissect_ncsi_ev_resp);
+  reg_ncsi_cmd(NCSI_CMD_DV, dissect_ncsi_dv_req, dissect_ncsi_dv_resp);
+  reg_ncsi_cmd(NCSI_CMD_SMA, dissect_ncsi_sma_req, dissect_ncsi_sma_resp);
+  reg_ncsi_cmd(NCSI_CMD_EBF, dissect_ncsi_ebf_req, dissect_ncsi_ebf_resp);
+  reg_ncsi_cmd(NCSI_CMD_DBF, dissect_ncsi_dbf_req, dissect_ncsi_dbf_resp);
+  reg_ncsi_cmd(NCSI_CMD_EGMF, dissect_ncsi_egmf_req, dissect_ncsi_egmf_resp);
+  reg_ncsi_cmd(NCSI_CMD_DGMF, dissect_ncsi_dgmf_req, dissect_ncsi_dgmf_resp);
+  reg_ncsi_cmd(NCSI_CMD_SFC, dissect_ncsi_sfc_req, dissect_ncsi_sfc_resp);
+  reg_ncsi_cmd(NCSI_CMD_GVID, dissect_ncsi_gvid_req, dissect_ncsi_gvid_resp);
+  reg_ncsi_cmd(NCSI_CMD_GC, dissect_ncsi_gc_req, dissect_ncsi_gc_resp);
+  reg_ncsi_cmd(NCSI_CMD_GP, dissect_ncsi_gp_req, dissect_ncsi_gp_resp);
+  reg_ncsi_cmd(NCSI_CMD_GCPS, dissect_ncsi_gcps_req, dissect_ncsi_gcps_resp);
+  reg_ncsi_cmd(NCSI_CMD_GS, dissect_ncsi_gs_req, dissect_ncsi_gs_resp);
+  reg_ncsi_cmd(NCSI_CMD_GPS, dissect_ncsi_gps_req, dissect_ncsi_gps_resp);
+  reg_ncsi_cmd(NCSI_CMD_OEM, dissect_ncsi_oem_req, dissect_ncsi_oem_resp);
 }
 
 /*
